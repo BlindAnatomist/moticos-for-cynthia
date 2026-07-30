7837ed61f66a84a206fa6ed3d5c5053f901a4e42
BlindAnatomist <132391374+BlindAnatomist@users.noreply.github.com>
2026-07-30 11:24:58 -0500

    Load iPhone-first layout rules

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MoticosMerge from "./MoticosMerge.jsx";
 import "./styles.css";
+import "./iphone.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
